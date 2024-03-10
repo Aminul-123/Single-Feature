1a97f958a00dfe52fa609b33a040370686905652
Aminul Ali <145538090+Aminul-123@users.noreply.github.com>
2024-03-10 20:25:34 +0530

    Added via upload

---
+++ New-stack/app.js
@@ -0,0 +1,6 @@
+const log=document.querySelector(".log-n-sign");
+const log_in=document.querySelector(".log-in");
+
+log_in.addEventListener("click",()=> {
+    log_in.classList.add("log-in-modal");
+})
